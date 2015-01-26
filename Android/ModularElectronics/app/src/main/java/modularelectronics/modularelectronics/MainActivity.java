eec9fd5d80abeee962d9e04b89b2676a1568a838
dKalicki <danielkalicki@gmail.com>
2015-01-26 22:03:16 +0100

    Main_activity interface changed 2.

--- Android/ModularElectronics/app/src/main/java/modularelectronics/modularelectronics/MainActivity.java
+++ Android/ModularElectronics/app/src/main/java/modularelectronics/modularelectronics/MainActivity.java
@@ -2,8 +2,8 @@ package modularelectronics.modularelectronics;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,14 +12,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        TextView bluetoothLETerminal_text = (TextView)findViewById(R.id.BluetoothLETerminal_text);
+        bluetoothLETerminal_text.setText("Test");
     }
 
     @Override
