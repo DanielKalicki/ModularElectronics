62cd9e3a1ecb5c0c5b89bba40276f5c7f988dfa7
dKalicki <danielkalicki@gmail.com>
2015-01-27 19:23:05 +0100

    Android software - basic Bluetooth LE Scan added

--- Android/ModularElectronics/app/src/main/java/modularelectronics/modularelectronics/MainActivity.java
+++ Android/ModularElectronics/app/src/main/java/modularelectronics/modularelectronics/MainActivity.java
@@ -1,22 +1,105 @@
 package modularelectronics.modularelectronics;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.bluetooth.le.BluetoothLeScanner;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
 
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+
+    private final static int REQUEST_ENABLE_BT = 1;
+    private static final long SCAN_PERIOD=10_000;
+
+    TextView bleTerminal_text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setTitle("ActionBar");
+        mHandler=new Handler();
+
         setContentView(R.layout.activity_main);
 
+        // Use this check to determine whether BLE is supported on the device.  Then you can
+        // selectively disable BLE-related features.
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)){
+            Toast.makeText(this,R.string.ble_not_supported,Toast.LENGTH_SHORT).show();
+            finish();
+        }
 
-        TextView bluetoothLETerminal_text = (TextView)findViewById(R.id.BluetoothLETerminal_text);
-        bluetoothLETerminal_text.setText("Test");
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        bleTerminal_text = (TextView)findViewById(R.id.ble_terminal_text);
+        bleTerminal_text.setText("Test");
+
+        scanLeDevice(true);
     }
+
+    private void scanLeDevice(final boolean enable) {
+        if(enable){
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning=false;
+                    mBluetoothAdapter.startLeScan(mLeScanCallback);
+                    invalidateOptionsMenu();
+                }
+            },SCAN_PERIOD);
+
+            mScanning=true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+
+        } else {
+            mScanning=false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+        invalidateOptionsMenu();
+    }
+
+    // Device scan callback.
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            bleTerminal_text.setText("Found");
+                        }
+                    });
+                }
+            };
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
