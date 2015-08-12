9d563d478c3a977a1953d2a932e084e24f50aad3
dKalicki <danielkalicki@gmail.com>
2015-08-12 21:22:54 +0200

    Code - User interface module - Registers set/get function written up to register 0x1D (Independent Sink Current Time)

--- Code/UserInterface/src/icDrivers/ADP8866/ADP8866_reg.c
+++ Code/UserInterface/src/icDrivers/ADP8866/ADP8866_reg.c
@@ -33,7 +33,6 @@ typedef enum {
 	ADP8866_LED_POWER_SOURCE_SEL_1_REG					= 0x09,
 	ADP8866_LED_POWER_SOURCE_SEL_2_REG					= 0x0A,
 
-
 	ADP8866_BACKLIGHT_CONF_REG							= 0x10,
 	ADP8866_BACKLIGHT_SEL_REG							= 0x11,
 	ADP8866_BACKLIGHT_FADE_REG							= 0x12,
@@ -88,5 +87,252 @@ static void ADP8866_GetRegister(ADP8866_Registers_t ADP8866_reg, uint8_t *val)
 	i2c_RegisterGet(I2C0, ADP8866_ADDR, (uint8_t)ADP8866_reg, val);
 }
 
+#define ADP8866_INT_CFG_SHIFT	6
+#define ADP8866_NSTBY_SHIFT		5
+#define ADP8866_ALT_GSEL_SHIFT	4
+#define ADP8866_GDWN_DIS_SHIFT	3
+#define ADP8866_SIS_EN_SHIFT	2
+#define ADP8866_BL_EN_SHIFT		0
+#define ADP8866_INT_CFG_MASK	0x01
+#define ADP8866_NSTBY_MASK		0x01
+#define ADP8866_ALT_GSEL_MASK	0x01
+#define ADP8866_GDWN_DIS_MASK	0x01
+#define ADP8866_SIS_EN_MASK		0x01
+#define ADP8866_BL_EN_MASK		0x01
+void ADP8866_SetModeControl(ADP8866_ModeControl_t mode)
+{
+	uint8_t val = ((mode.INT_CFG  & ADP8866_INT_CFG_MASK)  << ADP8866_INT_CFG_SHIFT)
+			    + ((mode.NSTBY    & ADP8866_NSTBY_MASK)    << ADP8866_NSTBY_SHIFT)
+			    + ((mode.ALT_GSEL & ADP8866_ALT_GSEL_MASK) << ADP8866_ALT_GSEL_SHIFT)
+			    + ((mode.GDWN_DIS & ADP8866_GDWN_DIS_MASK) << ADP8866_GDWN_DIS_SHIFT)
+			    + ((mode.SIS_EN   & ADP8866_SIS_EN_MASK)   << ADP8866_SIS_EN_SHIFT)
+			    + ((mode.BL_EN    & ADP8866_BL_EN_MASK)    << ADP8866_BL_EN_SHIFT);
+	ADP8866_SetRegister(ADP8866_MODE_CONTROL_REG, val);
+}
 
-void ADP8866_SetModeControl()
+#define ADP8866_ISCOFF_INT_SHIFT	6
+#define ADP8866_BLOFF_INT_SHIFT		5
+#define ADP8866_SHORT_INT_SHIFT		4
+#define ADP8866_TSD_INT_SHIFT		3
+#define ADP8866_OVP_INT_SHIFT		2
+#define ADP8866_BLOFF_INT_MASK		0x01
+#define ADP8866_SHORT_INT_MASK		0x01
+#define ADP8866_TSD_INT_MASK		0x01
+#define ADP8866_ISCOFF_INT_MASK		0x01
+#define ADP8866_OVP_INT_MASK		0x01
+void ADP8866_SetInterruptStatus(ADP8866_InterruptStatus_t status)
+{
+	uint8_t val = ((status.BLOFF_INT  & ADP8866_BLOFF_INT_MASK)  << ADP8866_ISCOFF_INT_SHIFT)
+			    + ((status.SHORT_INT  & ADP8866_SHORT_INT_MASK)  << ADP8866_BLOFF_INT_SHIFT)
+			    + ((status.TSD_INT    & ADP8866_TSD_INT_MASK)    << ADP8866_SHORT_INT_SHIFT)
+			    + ((status.ISCOFF_INT & ADP8866_ISCOFF_INT_MASK) << ADP8866_TSD_INT_SHIFT)
+			    + ((status.OVP_INT    & ADP8866_OVP_INT_MASK)    << ADP8866_OVP_INT_SHIFT);
+	ADP8866_SetRegister(ADP8866_INTERRUPT_STATUS_REG, val);
+}
+
+
+#define ADP8866_ISCOFF_IEN_SHIFT	6
+#define ADP8866_BLOFF_IEN_SHIFT		5
+#define ADP8866_SHORT_IEN_SHIFT		4
+#define ADP8866_TSD_IEN_SHIFT		3
+#define ADP8866_OVP_IEN_SHIFT		2
+#define ADP8866_BLOFF_IEN_MASK		0x01
+#define ADP8866_SHORT_IEN_MASK		0x01
+#define ADP8866_TSD_IEN_MASK		0x01
+#define ADP8866_ISCOFF_IEN_MASK		0x01
+#define ADP8866_OVP_IEN_MASK		0x01
+void ADP8866_SetInterruptEnable(ADP8866_InterruptEnable_t interruptEnable)
+{
+	uint8_t val = ((interruptEnable.BLOFF_IEN  & ADP8866_BLOFF_IEN_MASK)  << ADP8866_ISCOFF_IEN_SHIFT)
+			    + ((interruptEnable.SHORT_IEN  & ADP8866_SHORT_IEN_MASK)  << ADP8866_BLOFF_IEN_SHIFT)
+			    + ((interruptEnable.TSD_IEN    & ADP8866_TSD_IEN_MASK)    << ADP8866_SHORT_IEN_SHIFT)
+			    + ((interruptEnable.ISCOFF_IEN & ADP8866_ISCOFF_IEN_MASK) << ADP8866_TSD_IEN_SHIFT)
+			    + ((interruptEnable.OVP_IEN    & ADP8866_OVP_IEN_MASK)    << ADP8866_OVP_IEN_SHIFT);
+	ADP8866_SetRegister(ADP8866_INTERRUPT_ENABLE_REG, val);
+}
+
+void ADP8866_SetIndependSinkInterruptSelection(uint16_t diodesList)
+{
+	uint8_t IndepSinkInt_reg1 = 0x00;
+	/* If the ninth bit is set then include Diode 9 */
+	if ((diodesList & 0x200) == 1)
+	{
+		IndepSinkInt_reg1 |= 0x01;
+	}
+	ADP8866_SetRegister(ADP8866_INDEPENDENT_SINK_INT_SEL_1_REG, IndepSinkInt_reg1);
+
+	uint8_t IndepSinkInt_reg2 = 0x00;
+	for (uint16_t i = 1; i < 9; i++)
+	{
+		if ((diodesList & (1U << i)) == 1)
+		{
+			IndepSinkInt_reg2 |= 1U << (i - 1);
+		}
+	}
+	ADP8866_SetRegister(ADP8866_INDEPENDENT_SINK_INT_SEL_2_REG, IndepSinkInt_reg2);
+}
+
+
+#define ADP8866_GAIN_MODE_SHIFT 		0
+#define ADP8866_GAIN_MODE_LIMIT_SHIFT 	2
+#define ADP8866_GAIN_MODE_MASK			0x03
+#define ADP8866_GAIN_MODE_LIMIT_MASK	0x01
+void ADP8866_SetChargePumpGain(ADP8866_GainMode_t gainMode, ADP8866_GainModeLimit_t gainModeLimit)
+{
+	uint8_t val = 0x00;
+	val = (((uint8_t)gainMode 	   & ADP8866_GAIN_MODE_MASK) 	   << ADP8866_GAIN_MODE_SHIFT)
+		+ (((uint8_t)gainModeLimit & ADP8866_GAIN_MODE_LIMIT_MASK) << ADP8866_GAIN_MODE_LIMIT_SHIFT);
+	ADP8866_SetRegister(ADP8866_CHARGE_PUMP_GAIN_REG, val);
+}
+
+#define ADP8866_D9LVL_SHIFT		6
+#define ADP8866_LEVEL_SET_SHIFT	0
+#define ADP8866_D9LVL_MASK		0x01
+#define ADP8866_LEVEL_SET_MASK	0x3F
+void ADP8866_SetOutputLevel(uint16_t diodesLevelSelect, ADP8866_MaxCurrentRange_t maxCurrentRange)
+{
+	uint8_t outputLevel_reg1 = 0x00;
+	if ((diodesLevelSelect & 0x200) == 1)
+	{
+		outputLevel_reg1 |= (ADP8866_D9LVL_MASK << ADP8866_D9LVL_SHIFT);
+	}
+	outputLevel_reg1 |= (((uint8_t)maxCurrentRange & ADP8866_LEVEL_SET_MASK) << ADP8866_LEVEL_SET_SHIFT);
+	ADP8866_SetRegister(ADP8866_OUTPUT_LEVEL_SEL_1_REG, val);
+
+	uint8_t outputLevel_reg2 = 0x00;
+	for (uint16_t i = 1; i < 9; i++)
+	{
+		if ((diodesLevelSelect & (1U << i)) == 1)
+		{
+			outputLevel_reg2 |= 1U << (i - 1);
+		}
+	}
+	ADP8866_SetRegister(ADP8866_OUTPUT_LEVEL_SEL_2_REG, outputLevel_reg2);
+}
+
+void ADP8866_SetLedPowerSource(uint16_t diodesList)
+{
+	uint8_t LedPowerSource_reg1 = 0x00;
+	/* If the ninth bit is set then include Diode 9 */
+	if ((diodesList & 0x200) == 1)
+	{
+		LedPowerSource_reg1 |= 0x01;
+	}
+	ADP8866_SetRegister(ADP8866_LED_POWER_SOURCE_SEL_1_REG, LedPowerSource_reg1);
+
+	uint8_t LedPowerSource_reg2 = 0x00;
+	for (uint16_t i = 1; i < 9; i++)
+	{
+		if ((diodesList & (1U << i)) == 1)
+		{
+			LedPowerSource_reg2 |= 1U << (i - 1);
+		}
+	}
+	ADP8866_SetRegister(ADP8866_LED_POWER_SOURCE_SEL_1_REG, LedPowerSource_reg2);
+}
+
+#define ADP8866_D9SEL_SHIFT		4
+#define ADP8866_CABCFADE_SHIFT	3
+#define ADP8866_BL_LAW_SHIFT	1
+#define ADP8866_D9SEL_MASK		0x01
+#define ADP8866_CABCFADE_MASK	0x01
+#define ADP8866_BL_LAW_MASK		0x03
+void ADP8866_SetBacklightConfiguration(uint16_t diodesList, ADP8866_Backlight_respond_to_change_t backlightResponse, ADP8866_Transfer_Law_Function_t backlightTransferLaw)
+{
+	uint8_t BacklightConfiguration_reg1 = 0x00;
+	/* If the ninth bit is set then include Diode 9 */
+	if ((diodesList & 0x200) == 1)
+	{
+		BacklightConfiguration_reg1 |= (ADP8866_D9SEL_MASK << ADP8866_D9SEL_SHIFT);
+	}
+	BacklightConfiguration_reg1 |= (((uint8_t)backlightResponse    & ADP8866_CABCFADE_MASK) << ADP8866_CABCFADE_SHIFT)
+								 + (((uint8_t)backlightTransferLaw & ADP8866_BL_LAW_MASK)   << ADP8866_BL_LAW_SHIFT);
+	ADP8866_SetRegister(ADP8866_BACKLIGHT_CONF_REG, BacklightConfiguration_reg1);
+
+	uint8_t BacklightConfiguration_reg2 = 0x00;
+	for (uint16_t i = 1; i < 9; i++)
+	{
+		if ((diodesList & (1U << i)) == 1)
+		{
+			BacklightConfiguration_reg2 |= 1U << (i - 1);
+		}
+	}
+	ADP8866_SetRegister(ADP8866_BACKLIGHT_SEL_REG, BacklightConfiguration_reg2);
+}
+
+#define ADP8866_BL_FO_SHIFT		4
+#define ADP8866_BL_FI_SHIFT		0
+#define ADP8866_BL_FO_MAX		0x0F
+#define ADP8866_BL_FI_MASK		0x0F
+void ADP8866_SetBacklightFade(ADP8866_Backlight_Fade_Rate_t fadeOut, ADP8866_Backlight_Fade_Rate_t fadeIn)
+{
+	uint8_t val = 0x00;
+	val = (((uint8_t)fadeOut & ADP8866_BL_FO_MAX) << ADP8866_BL_FO_SHIFT)
+		+ (((uint8_t)fadeIn  & ADP8866_BL_FI_MASK) << ADP8866_BL_FI_SHIFT);
+
+	ADP8866_SetRegister(ADP8866_BACKLIGHT_FADE_REG, val);
+}
+
+#define ADP8866_BL_MC_SHIFT		0
+#define ADP8866_BL_MC_MAX		0x7F
+void ADP8866_SetBacklightMaxCurrent(uint8_t BacklightSetting)
+{
+	uint8_t val;
+	val = (BacklightSetting & 0x7F) << ADP8866_BL_MC_SHIFT;
+	ADP8866_SetRegister(ADP8866_BACKLIGHT_MAX_CURRENT_REG, val);
+}
+
+#define ADP8866_D9_EN_SHIFT		2
+#define ADP8866_SC_LAW_SHIFT	0
+#define ADP8866_D9_EN_MASK		0x01
+#define ADP8866_SC_LAW_MASK		0x03
+void ADP8866_SetIndependentSinkCurrentControl(uint16_t diodesList, ADP8866_Transfer_Law_Function_t transferLawFunction)
+{
+	uint8_t IndependentSinkCurrent_EnableAct_reg1 = 0x00;
+	/* If the ninth bit is set then include Diode 9 */
+	if ((diodesList & 0x200) == 1)
+	{
+		IndependentSinkCurrent_EnableAct_reg1 |= (ADP8866_D9_EN_MASK << ADP8866_D9_EN_SHIFT);
+	}
+	IndependentSinkCurrent_EnableAct_reg1 |= (((uint8_t)transferLawFunction & ADP8866_SC_LAW_MASK)   << ADP8866_SC_LAW_SHIFT);
+	ADP8866_SetRegister(ADP8866_INDEPENDENT_SINK_CURRENT_CTR_1_REG, IndependentSinkCurrent_EnableAct_reg1);
+
+	uint8_t IndependentSinkCurrent_EnableAct_reg2 = 0x00;
+	for (uint16_t i = 1; i < 9; i++)
+	{
+		if ((diodesList & (1U << i)) == 1)
+		{
+			IndependentSinkCurrent_EnableAct_reg2 |= 1U << (i - 1);
+		}
+	}
+	ADP8866_SetRegister(ADP8866_INDEPENDENT_SINK_CURRENT_CTR_2_REG, IndependentSinkCurrent_EnableAct_reg2);
+}
+
+#define ADP8866_SCON_SHIFT		4
+#define ADP8866_SC5OFF_SHIFT	0
+#define ADP8866_SC4OFF_SHIFT	6
+#define ADP8866_SC3OFF_SHIFT	4
+#define ADP8866_SC2OFF_SHIFT	2
+#define ADP8866_SC1OFF_SHIFT	0
+
+#define ADP8866_SCON_MASK		0x0F
+#define ADP8866_SC5OFF_MASK		0x03
+#define ADP8866_SC4OFF_MASK		0x03
+#define ADP8866_SC3OFF_MASK		0x03
+#define ADP8866_SC2OFF_MASK		0x03
+#define ADP8866_SC1OFF_MASK		0x03
+void ADP8866_SetIndependentSinkCurrentTime(ADP8866_SC_Time_t scTime, ADP8866_ScOffTime_t Sc5Off, ADP8866_ScOffTime_t Sc4Off,
+										 ADP8866_ScOffTime_t Sc3Off, ADP8866_ScOffTime_t Sc2Off, ADP8866_ScOffTime_t Sc1Off)
+{
+	uint8_t IndepSinkCurrTime_reg1 = 0x00;
+	IndepSinkCurrTime_reg1 = (((uint8_t)scTime & ADP8866_SCON_MASK)   << ADP8866_SCON_SHIFT)
+						   + (((uint8_t)Sc5Off & ADP8866_SC5OFF_MASK) << ADP8866_SC5OFF_SHIFT);
+	ADP8866_SetRegister(ADP8866_INDEPENDENT_SINK_CURRENT_TIME_1_REG, IndepSinkCurrTime_reg1);
+
+	uint8_t IndepSinkCurrTime_reg2 = 0x00;
+	IndepSinkCurrTime_reg2 = (((uint8_t)Sc4Off & ADP8866_SC4OFF_MASK) << ADP8866_SC4OFF_SHIFT)
+						   + (((uint8_t)Sc3Off & ADP8866_SC3OFF_MASK) << ADP8866_SC3OFF_SHIFT)
+						   + (((uint8_t)Sc2Off & ADP8866_SC2OFF_MASK) << ADP8866_SC2OFF_SHIFT)
+						   + (((uint8_t)Sc1Off & ADP8866_SC1OFF_MASK) << ADP8866_SC1OFF_SHIFT);
+	ADP8866_SetRegister(ADP8866_INDEPENDENT_SINK_CURRENT_TIME_2_REG, IndepSinkCurrTime_reg2);
+}
--- Code/UserInterface/src/icDrivers/ADP8866/ADP8866_reg.h
+++ Code/UserInterface/src/icDrivers/ADP8866/ADP8866_reg.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+/* Comments included in this code comes from the ic datasheet:
+ * http://www.analog.com/media/en/technical-documentation/data-sheets/ADP8866.pdf
+ */
+
 typedef struct{
 	/*
 	 * Interrupt configuration.
@@ -43,5 +47,235 @@ typedef struct{
 	uint8_t BL_EN;
 } ADP8866_ModeControl_t;
 void ADP8866_SetModeControl(ADP8866_ModeControl_t mode);
+
+typedef struct{
+	/*
+	 * Independent sink off.
+		1 = indicates that the controller has ramped all the independent sinks designated in Register 0x04 and
+			Register 0x05 to off.
+		0 = the controller has not ramped all designated independent sinks to off.
+	 */
+	uint8_t ISCOFF_INT;
+	/*
+	 * Backlight off.
+		1 = indicates that the controller has faded the backlight sinks to off.
+		0 = the controller has not completed fading the backlight sinks to off
+	 */
+	uint8_t BLOFF_INT;
+	/*
+	 * Short-circuit error.
+		1 = a short-circuit or overload condition on VOUT or current sinks was detected.
+		0 = no short-circuit or overload condition was detected.
+	 */
+	uint8_t SHORT_INT;
+	/*
+	 * Thermal shutdown.
+		1 = device temperature is too high and has been shut down.
+		0 = no overtemperature condition was detected.
+	 */
+	uint8_t TSD_INT;
+	/*
+	 * Overvoltage interrupt.
+		1 = charge-pump output voltage has exceeded VOVP.
+		0 = charge-pump output voltage has not exceeded VOVP.
+	 */
+	uint8_t OVP_INT;
+}ADP8866_InterruptStatus_t;
+void ADP8866_SetInterruptStatus(ADP8866_InterruptStatus_t status);
+
+typedef struct{
+	/*
+	 * Automated ISC off indicator.
+		1 = the automated independent sink off indicator is enabled.
+		0 = the automated independent sink off indicator is disabled.
+	 */
+	uint8_t ISCOFF_IEN;
+	/*
+	 * Automated backlight off indicator.
+		1 = the automated backlight off indicator is enabled.
+		0 = the automated backlight off indicator is disabled.
+		When this bit is set, an INT is generated anytime that a backlight fade-out is over. This occurs after an automated
+		fade-out or after the completion of a backlight dimming profile. This is useful to synchronize the complete turn off
+		for the backlights with other devices in the application.
+	 */
+	uint8_t BLOFF_IEN;
+	/*
+	 * Short-circuit interrupt enabled. When the SHORT_INT status bit is set after an error condition, an interrupt is raised
+			to the host if the SHORT_IEN flag is enabled.
+		1 = the short-circuit interrupt is enabled.
+		0 = the short-circuit interrupt is disabled (SHORT_INT flag is still asserted).
+	 */
+	uint8_t SHORT_IEN;
+	/*
+	 * Thermal shutdown interrupt enabled. When the TSD_INT status bit is set after an error condition, an interrupt is
+			raised to the host if the TSD_IEN flag is enabled.
+		1 = the thermal shutdown interrupt is enabled.
+		0 = the thermal shutdown interrupt is disabled (TSD_INT flag is still asserted).
+	 */
+	uint8_t TSD_IEN;
+	/*
+	 * Overvoltage interrupt enabled. When the OVP_INT status bit is set after an error condition, an interrupt is raised to
+			the host if the OVP_IEN flag is enabled.
+		1 = the overvoltage interrupt is enabled.
+		0 = the overvoltage interrupt is disabled (OVP_INT flag is still asserted)
+	 */
+	uint8_t OVP_IEN;
+}ADP8866_InterruptEnable_t;
+void ADP8866_SetInterruptEnable(ADP8866_InterruptEnable_t interruptEnable);
+
+/* To include diode [X] in the ISCOFF_INT flag set bit [X] as 1, to exclude it set the bit [X] to 0 */
+void ADP8866_SetIndependSinkInterruptSelection(uint16_t diodesList);
+
+typedef enum{
+	ADP8866_GAIN_MODE_AUTO	=	0,
+	ADP8866_GAIN_MODE_1X	=	1,
+	ADP8866_GAIN_MODE_1_5X	=	2,
+	ADP8866_GAIN_MODE_2X	=	3
+}ADP8866_GainMode_t;
+void ADP8866_SetChargePumpGain(ADP8866_GainMode_t gainMode, ADP8866_GainModeLimit_t gainModeLimit);
+
+typedef enum{
+	ADP_MAX_CURR_RANGE_31_3_mA	= 0,
+	ADP_MAX_CURR_RANGE_27_8_mA	= 1,
+	ADP_MAX_CURR_RANGE_25_0_mA	= 2,
+	ADP_MAX_CURR_RANGE_22_7_mA	= 3,
+	ADP_MAX_CURR_RANGE_20_8_mA	= 4,
+	ADP_MAX_CURR_RANGE_19_2_mA	= 5,
+	ADP_MAX_CURR_RANGE_17_9_mA	= 6,
+	ADP_MAX_CURR_RANGE_16_7_mA	= 7,
+	ADP_MAX_CURR_RANGE_15_6_mA	= 8,
+	ADP_MAX_CURR_RANGE_14_7_mA	= 9,
+	ADP_MAX_CURR_RANGE_13_9_mA	= 10,
+	ADP_MAX_CURR_RANGE_13_2_mA	= 11,
+	ADP_MAX_CURR_RANGE_12_5_mA	= 12,
+	ADP_MAX_CURR_RANGE_11_9_mA	= 13,
+	ADP_MAX_CURR_RANGE_11_4_mA	= 14,
+	ADP_MAX_CURR_RANGE_10_9_mA	= 15,
+	ADP_MAX_CURR_RANGE_10_4_mA	= 16,
+	ADP_MAX_CURR_RANGE_10_0_mA	= 17,
+	ADP_MAX_CURR_RANGE_9_62_mA	= 18,
+	ADP_MAX_CURR_RANGE_9_26_mA	= 19,
+	ADP_MAX_CURR_RANGE_8_93_mA	= 20,
+	ADP_MAX_CURR_RANGE_8_62_mA	= 21,
+	ADP_MAX_CURR_RANGE_8_33_mA	= 22,
+	ADP_MAX_CURR_RANGE_8_06_mA	= 23,
+	ADP_MAX_CURR_RANGE_7_81_mA	= 24,
+	ADP_MAX_CURR_RANGE_7_58_mA	= 25,
+	ADP_MAX_CURR_RANGE_7_35_mA	= 26,
+	ADP_MAX_CURR_RANGE_7_14_mA	= 27,
+	ADP_MAX_CURR_RANGE_6_94_mA	= 28,
+	ADP_MAX_CURR_RANGE_6_76_mA	= 29,
+	ADP_MAX_CURR_RANGE_6_58_mA	= 30,
+	ADP_MAX_CURR_RANGE_6_41_mA	= 31,
+	ADP_MAX_CURR_RANGE_6_25_mA	= 32,
+	ADP_MAX_CURR_RANGE_6_10_mA	= 33,
+	ADP_MAX_CURR_RANGE_5_95_mA	= 34,
+	ADP_MAX_CURR_RANGE_5_81_mA	= 35,
+	ADP_MAX_CURR_RANGE_5_68_mA	= 36,
+	ADP_MAX_CURR_RANGE_5_56_mA	= 37,
+	ADP_MAX_CURR_RANGE_5_43_mA	= 38,
+	ADP_MAX_CURR_RANGE_5_32_mA	= 39,
+	ADP_MAX_CURR_RANGE_5_21_mA	= 40,
+	ADP_MAX_CURR_RANGE_5_10_mA	= 41,
+	ADP_MAX_CURR_RANGE_5_00_mA	= 42,
+	ADP_MAX_CURR_RANGE_4_90_mA	= 43,
+	ADP_MAX_CURR_RANGE_4_81_mA	= 44,
+	ADP_MAX_CURR_RANGE_4_72_mA	= 45,
+	ADP_MAX_CURR_RANGE_4_63_mA	= 46,
+	ADP_MAX_CURR_RANGE_4_55_mA	= 47,
+	ADP_MAX_CURR_RANGE_4_46_mA	= 48,
+	ADP_MAX_CURR_RANGE_4_39_mA	= 49,
+	ADP_MAX_CURR_RANGE_4_31_mA	= 50,
+	ADP_MAX_CURR_RANGE_4_24_mA	= 51,
+	ADP_MAX_CURR_RANGE_4_17_mA	= 52,
+	ADP_MAX_CURR_RANGE_4_10_mA	= 53,
+	ADP_MAX_CURR_RANGE_4_03_mA	= 54,
+	ADP_MAX_CURR_RANGE_3_97_mA	= 55,
+	ADP_MAX_CURR_RANGE_3_91_mA	= 56,
+	ADP_MAX_CURR_RANGE_3_85_mA	= 57,
+	ADP_MAX_CURR_RANGE_3_79_mA	= 58,
+	ADP_MAX_CURR_RANGE_3_73_mA	= 59,
+	ADP_MAX_CURR_RANGE_3_68_mA	= 60,
+	ADP_MAX_CURR_RANGE_3_62_mA	= 61,
+	ADP_MAX_CURR_RANGE_3_57_mA	= 62,
+	ADP_MAX_CURR_RANGE_PWM		= 63
+} ADP8866_MaxCurrentRange_t;
+/* To control diode [X] with LEVEL_SET bits set bit [X] as 1,
+ * to set diode [X] to normal mode (25mA full-scale current) set the bit [X] to 0 */
+void ADP8866_SetOutputLevel(ADP8866_DiodeLevelMode_t diodeMode, ADP8866_MaxCurrentRange_t maxCurrentRange);
+
+/* To power LED diode [X] from battery set bit [X] as 1,
+ * to power LED diode [X] from the charge pump set the bit [X] to 0 */
+void ADP8866_SetLedPowerSource(uint16_t diodesList);
+
+typedef enum{
+	ADP8866_BACKLIGHT_FADE				= 0,
+	ADP8866_BACKLIGHT_INSTANT_TRANSITION = 1
+} ADP8866_Backlight_respond_to_change_t;
+typedef enum{
+	ADP8866_TRANSF_SQUARE_LAW_LINEAR_TIME_STEPS = 0,
+	ADP8866_TRANSF_SQUARE_LAW_NONLINEAR_TIME_STEPS_CUBIC10 = 2,
+	ADP8866_RANSF_SQUARE_LAW_NONLINEAR_TIME_STEPS_CUBIC11 = 3,
+} ADP8866_Transfer_Law_Function_t;
+
+/* To select LED diode [X] as part of the independent sink group set bit [X] as 1,
+ * to select LED diode [X] as part of the backlight group set the bit [X] to 0 */
+void ADP8866_SetBacklightConfiguration(uint16_t diodesList, ADP8866_Backlight_respond_to_change_t backlightResponse, ADP8866_Transfer_Law_Function_t backlightTransferLaw);
+
+typedef enum{
+	ADP8866_BACKLIGHT_FADE_DISABLE	= 0,
+	ADP8866_BACKLIGHT_FADE_0_05s	= 1,
+	ADP8866_BACKLIGHT_FADE_0_10s	= 2,
+	ADP8866_BACKLIGHT_FADE_0_15s	= 3,
+	ADP8866_BACKLIGHT_FADE_0_20s	= 4,
+	ADP8866_BACKLIGHT_FADE_0_25s	= 5,
+	ADP8866_BACKLIGHT_FADE_0_30s	= 6,
+	ADP8866_BACKLIGHT_FADE_0_35s	= 7,
+	ADP8866_BACKLIGHT_FADE_0_40s	= 8,
+	ADP8866_BACKLIGHT_FADE_0_45s	= 9,
+	ADP8866_BACKLIGHT_FADE_0_50s	= 10,
+	ADP8866_BACKLIGHT_FADE_0_75s	= 11,
+	ADP8866_BACKLIGHT_FADE_1_00s	= 12,
+	ADP8866_BACKLIGHT_FADE_1_25s	= 13,
+	ADP8866_BACKLIGHT_FADE_1_50s	= 14,
+	ADP8866_BACKLIGHT_FADE_1_75s	= 15,
+}ADP8866_Backlight_Fade_Rate_t;
+void ADP8866_SetBacklightFade(ADP8866_Backlight_Fade_Rate_t fadeOut, ADP8866_Backlight_Fade_Rate_t fadeIn);
+
+/* Because the maximum current is calculated using a square law function and is a function of Backlight Output Level
+ * arguments for this c function are not enumerated the user needs to read the correct register settign from the
+ * datasheet and set the uint8_t BacklightSetting to the corresponding DAC code value*/
+void ADP8866_SetBacklightMaxCurrent(uint8_t BacklightSetting);
+
+/* To enable act on LED diode [X] set bit [X] as 1,
+ * to disable act on LED diode [X] set the bit [X] to 0 */
+void ADP8866_SetIndependentSinkCurrentControl(uint16_t diodesList, ADP8866_Transfer_Law_Function_t transferLawFunction);
+
+typedef enum{
+	ADP8866_SC_TIME_0_00s	=	0,
+	ADP8866_SC_TIME_0_05s	=	1,
+	ADP8866_SC_TIME_0_10s	=	2,
+	ADP8866_SC_TIME_0_15s	=	3,
+	ADP8866_SC_TIME_0_20s	=	4,
+	ADP8866_SC_TIME_0_25s	=	5,
+	ADP8866_SC_TIME_0_30s	=	6,
+	ADP8866_SC_TIME_0_35s	=	7,
+	ADP8866_SC_TIME_0_40s	=	8,
+	ADP8866_SC_TIME_0_45s	=	9,
+	ADP8866_SC_TIME_0_50s	=	10,
+	ADP8866_SC_TIME_0_55s	=	11,
+	ADP8866_SC_TIME_0_60s	=	12,
+	ADP8866_SC_TIME_0_65s	=	13,
+	ADP8866_SC_TIME_0_70s	=	14,
+	ADP8866_SC_TIME_0_75s	=	15
+} ADP8866_SC_Time_t;
+typedef enum{
+	ADP8866_SC_OFF_TIME_DISABLE	= 0,
+	ADP8866_SC_OFF_TIME_0_6s	= 1,
+	ADP8866_SC_OFF_TIME_1_2s	= 2,
+	ADP8866_SC_OFF_TIME_1_8s	= 3
+} ADP8866_ScOffTime_t;
+void ADP8866_SetIndependentSinkCurrentTime(ADP8866_SC_Time_t scTime, ADP8866_ScOffTime_t Sc5Off, ADP8866_ScOffTime_t Sc4Off,
+										 ADP8866_ScOffTime_t Sc3Off, ADP8866_ScOffTime_t Sc2Off, ADP8866_ScOffTime_t Sc1Off);
 
 #endif /* ADP8866_REG_H_ */
